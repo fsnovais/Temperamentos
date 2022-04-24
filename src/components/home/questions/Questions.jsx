0a5c6f5a32d485d24478b8c381db7a2508ffd760
Felipe Novais <felipekun213@gmail.com>
2022-04-24 16:56:14 -0300

    alteracao na logica para escolha

--- src/components/home/questions/Questions.jsx
+++ src/components/home/questions/Questions.jsx
@@ -1,53 +1,55 @@
 import "./questions.scss"
 import React, { useState, useEffect, useRef} from 'react';
-import { HashLink as Link } from 'react-router-hash-link';
+import { useHistory } from 'react-router-dom';
 
 
 export default function Questions({ data}) {
     const [error, setError] = useState('');
     const radiosWrapper = useRef();
-    const updateCheckedState = [];
-    const [temperament, SetTemperament] = useState('');
-
+    const [updateCheckedState, setUpdateCheckedState] = useState([]);
+    const history = useHistory();
+    console.log(updateCheckedState);
   
     useEffect(() => {
     radiosWrapper.current.querySelectorAll('input:checked').forEach( el => el.checked = false );
     }, [data]);
     
-    const changeHandler = (position) => {
-          if(!updateCheckedState.includes(position)){
-            updateCheckedState.push(position);
-        }else if(updateCheckedState.includes(position)){
-          const index = updateCheckedState.indexOf(position);
-          updateCheckedState.splice(index, 1);
-        }
+    const changeHandler = () => {
+        
         const sanguine = data.sanguine.filter(e => updateCheckedState.includes(e));
         const choleric = data.choleric.filter(e => updateCheckedState.includes(e));
         const melancholic = data.melancholic.filter(e => updateCheckedState.includes(e));
         const phlegmatic = data.phlegmatic.filter(e => updateCheckedState.includes(e));
 
         const result = Math.max(sanguine.length,choleric.length,melancholic.length,phlegmatic.length);
-        
+
         switch(result){
           case sanguine.length:
-            SetTemperament('/sanguine');
+            history.push('/sanguine');
+            console.log('entered in sanguine');
             break;
           case choleric.length:
-            SetTemperament('/choleric');
+            history.push('/choleric');
+            console.log('entered in choleric');
+
             break;
           case melancholic.length:
-            SetTemperament('/melancholic');
+            history.push('/melancholic');
+            console.log('entered in melancholic');
+
             break;
           case phlegmatic.length:
-            SetTemperament('/phlegmatic');
+            history.push('/phlegmatic');
+            console.log('entered in phlegmatic');
+
             break;
           default:
         }
-
-      if(error) {
-        setError('');
-      }
+        if(error) {
+          setError('');
+        }
     }
+
   
     return(
       <div className="card">
@@ -57,13 +59,13 @@ export default function Questions({ data}) {
             <div className="control" ref={radiosWrapper}>
               {data.choices.answer.map((choice, i) => (
                 <label className="radio has-background-light" key={i}>
-                  <input type="checkbox" name="answer" value={choice} onChange={() => changeHandler(i)} />
+                  <input type="checkbox" name="answer" value={choice} onChange={() => updateCheckedState.includes(i) ? setUpdateCheckedState(updateCheckedState.filter(e => e !== i)): setUpdateCheckedState( arr => [...arr, i])}/>
                   {choice}
                 </label>
               ))}
             </div>
             {error && <div className="has-text-danger">{error}</div>}
-            <Link className="link" to={temperament}>Resultado</Link>
+            <button className="link" onClick={changeHandler} >Resultado</button>
           </div>
         </div>
       </div>
